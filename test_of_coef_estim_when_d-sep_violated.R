2418f38b16dabce809200b240a27f29b4e96df62
amurrayw <swiftwind1230@gmail.com>
2017-07-04 14:33:38 +0200

    allowed for variable coefficents.

--- test_of_coef_estim_when_d-sep_violated.R
+++ test_of_coef_estim_when_d-sep_violated.R
@@ -1,12 +1,12 @@
 
 
-gen.data <- function(n.obs=1000){
+gen.data <- function(n.obs=1000, coef1=runif(n=1, min=-1, max=1), coef2=runif(n=1, min=-1, max=1)){
 
 
     ## This graph contains 3 nodes, with a collider at x2.
     x1 <- rnorm(n.obs)
     x3 <- rnorm(n.obs)
-    x2 <- rnorm(n.obs) + x1 + x3
+    x2 <- rnorm(n.obs) + x1*coef1 + x3*coef2
 
     return(data.frame(x1, x2, x3)) 
    
@@ -32,14 +32,61 @@ compare.models <- function(list.models){
     return(list(regress.diff=regress.diff, sd.diff=sd.diff))
 }
 
+##Coef of exactly one:
 
-results.vec <- replicate(compare.models(run.regression(gen.data(10000))), n=1000)
+results.vec <- replicate(compare.models(run.regression(gen.data(10000, 1, 1))), n=1000)
 
-par(mfrow=c(2,1))
+pdf("simple_lin_results.pdf")
 
-hist((unlist(results.vec[1,])), main="regression coef. diff")
+par(mfrow=c(2,2))
+
+hist((unlist(results.vec[1,])), main="regression coef. diff\n coef both 1")
 abline(v=0, col="red")
 
-hist((unlist(results.vec[2,])), main="sd coef. diff")
+hist((unlist(results.vec[2,])), main="sd coef. diff\n coef both 1")
 abline(v=0, col="red")
 
+
+
+
+## Positive coef. only.
+
+
+results.vec <- replicate(compare.models(run.regression(gen.data(10000, coef1=runif(n=1, min=0, max=1), coef2=runif(n=1, min=0, max=1)))), n=1000)
+
+
+hist((unlist(results.vec[1,])), main="regression coef. diff,\n pos. coef only", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+hist((unlist(results.vec[2,])), main="sd coef. diff", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+
+## Negitive coef. only.
+
+
+results.vec <- replicate(compare.models(run.regression(gen.data(10000, coef1=runif(n=1, min=-1, max=0), coef2=runif(n=1, min=-1, max=0)))), n=1000)
+
+##par(mfrow=c(2,1))
+
+hist((unlist(results.vec[1,])), main="regression coef. diff,\n neg. coef only", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+hist((unlist(results.vec[2,])), main="sd coef. diff,\n neg. coef only", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+
+## Both Positive and negative equally likely only.
+
+
+results.vec <- replicate(compare.models(run.regression(gen.data(10000, coef1=runif(n=1, min=-1, max=1), coef2=runif(n=1, min=-1, max=1)))), n=1000)
+
+##par(mfrow=c(2,1))
+
+hist((unlist(results.vec[1,])), main="regression coef. diff,\n both pos. and neg.", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+hist((unlist(results.vec[2,])), main="sd coef. diff,\n both pos. and neg.", xlab="diff. between correct and\n incorrect model coef.")
+abline(v=0, col="red")
+
+dev.off()
